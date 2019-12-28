dd5f1fe05762a61c2cc8ae25f2e36734e5a20b47
John Carlson <yottzumm@gmail.com>
2019-12-27 21:53:00 -0600

    removed recaptcha

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
 from app import app
 
-app.config['RECAPTCHA_USE_SSL'] = False
-app.config['RECAPTCHA_PUBLIC_KEY'] = 'public'
-app.config['RECAPTCHA_PRIVATE_KEY'] = 'private'
-app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
-
 app.run(host="0.0.0.0", port=8080, debug=True)
